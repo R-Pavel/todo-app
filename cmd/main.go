8a59857d1b7d6f4e5a660df0e617a28f094d443d
Pavel Rylskii <p.rylskii@platform.live>
2024-03-17 00:09:09 +0100

    feature: service, repository layers added

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+
+	"github.com/R-Pavel/todo-app"
+	"github.com/R-Pavel/todo-app/pkg/handler"
+	"github.com/R-Pavel/todo-app/pkg/repository"
+	"github.com/R-Pavel/todo-app/pkg/service"
+	"github.com/spf13/viper"
+)
+
 func main () {
-	
+	if err:= initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
-module github.com/R-Pavel/todo-app  
+module github.com/R-Pavel/todo-app
 
 go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
+	github.com/spf13/viper v1.18.2 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20240314144324-c7f7c6466f7f // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
